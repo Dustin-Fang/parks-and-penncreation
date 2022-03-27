53d3af548742253a6521b59329e524de04ef20d4
Dustin Fang <20fangd@gmail.com>
2022-03-27 16:26:16 -0400

    sql ddl changes:

--- ddl.sql
+++ ddl.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Species (
     SpeciesId           varchar(9),
-    CommonName          varchar(20),
     SpeciesOrder        varchar(16),
     Family              varchar(16),
     ScientificName      varchar(64),
@@ -12,35 +11,49 @@ CREATE TABLE Species (
     PRIMARY KEY (SpeciesId)
 );
 
-CREATE TABLE Parks (
-    Code        varchar(4),
-    Name        varchar(64),
-    Acres       int,
-    Latitude    decimal(2, 2),
-    Longitude   decimal(3, 2),
-    Zipcode     int,
-    PRIMARY KEY (Code)
+CREATE TABLE CommonNames (
+    CommonName          varchar(20),
+    SpeciesId           varchar(9),
+    PRIMARY KEY (CommonName),
+    FOREIGN KEY (SpeciesId) REFERENCES Species(SpeciesId)
 );
 
-CREATE TABLE LivesIn (
-    SpeciesId   varchar(9),
+CREATE TABLE Parks (
+    ParkId      int,
     ParkCode    varchar(4),
+    ParkName    varchar(64),
+    Acres       int,
+    Latitude    decimal(5, 2),
+    Longitude   decimal(5, 2),
+    PRIMARY KEY (ParkId)
+);
+
+CREATE TABLE Zipcode {
+    Zipcode     int,
+    ParkId      int,
+    PRIMARY KEY (Zipcode),
+    FOREIGN KEY (ParkId) REFERENCES Parks(ParkId)
+}
+
+CREATE TABLE FoundIn (
+    SpeciesId   varchar(9),
+    ParkId      int,
     FOREIGN KEY (SpeciesId) REFERENCES Species(SpeciesId),
-    FOREIGN KEY (ParkCode) REFERENCES Parks(Code)
+    FOREIGN KEY (ParkId) REFERENCES Parks(ParkId)
 );
 
 CREATE TABLE WeatherEvents (
     EventId         varchar(9),
-    ParkCode        varchar(4),
+    ParkID          int,
     Severity        varchar(10),
-    Precipitation   decimal(2, 2),
-    Latitude        decimal(2, 4),
-    Longitude       decimal(3, 4),
+    Precipitation   decimal(5, 2),
+    Latitude        decimal(7, 4),
+    Longitude       decimal(7, 4),
     Type            varchar(8),
     State           varchar(2),
     Zipcode         int,
     StartTime       datetime,
     EndTime         datetime,
     PRIMARY KEY (EventId),
-    FOREIGN KEY (ParkCode) REFERENCES Parks(Code)
+    FOREIGN KEY (ParkID) REFERENCES Parks(ParkID)
 );
