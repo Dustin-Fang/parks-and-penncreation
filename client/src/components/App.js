cfafaefadfdff510732c39d6d0284b4452d75f33
Shanna Edwards <shannaedwards@client0716-hnt.vpn.private.upenn.edu>
2022-04-20 17:01:31 -0400

    client setup

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+         Welcome to Parks n Penncreation!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
