2b1a2b17fd8325c6aebc4fcbbecd19de76a7464e
Shanna Edwards <shannaedwards@MacBook-Air-46.local>
2022-04-23 23:27:55 -0400

    homepage styling

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,16 +1,21 @@
+import {
+  Flex,
+  Image
+} from '@chakra-ui/react';
 import NavBar from './NavBar';
-import '../assets/HomePage.css';
 
 function HomePage() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Flex
+    width="100wh"  
+    height="100vh"  
+    justifyContent="center"
+    alignItems="center"
+    >
       <NavBar />
-        <h1>
-         Welcome to Parks n Penncreation!
-        </h1>
-      </header>
-    </div>
+      {/* placeholder, but would be cool if we can use getty api/web scraping to do different park pictures */}
+      <Image height="100%" width="100%" src='https://www.flushmate.com/sites/flushmate/files/2020-04/Sequoia-Lodge-Gallery-02.jpg'/>
+  </Flex>
   );
 }
 
